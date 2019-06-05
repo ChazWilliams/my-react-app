0f17bacdf6c5255db5ec6a8e26ad7c5b55001d73
Charlie Williams <charliewilliams@mac.com>
2019-06-05 15:47:37 +0100

    CSS Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to HummingbirdCRM</h1>
-    <p>The simple CRM to get business done.</p>
-    <Link to="/page-2/">Arrange Meeting</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to HummingbirdCRM</h1>
+        <p>The simple CRM to get business done.</p>
+        <Link to="/page-2/">Arrange Meeting</Link>
+      </div>
+    </div>
   </div>
 )
 
